59ae6f1998f7b505c6a481e70f37d679c17ac53e
mustafaghali <wad_alghali@notmail.com>
2016-11-21 15:18:23 +0000

    adding the main python script for caffe CNN test

---
+++ caffeTestScript.py
@@ -0,0 +1,25 @@
+
+# coding: utf-8
+
+# In[1]:
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+get_ipython().magic(u'matplotlib inline')
+
+
+caffe_root = './caffe-master/' 
+import sys
+sys.path.insert(0, caffe_root + 'python')
+import caffe
+
+plt.rcParams['figure.figsize'] = (10, 10)
+plt.rcParams['image.interpolation'] = 'nearest'
+plt.rcParams['image.cmap'] = 'gray'
+
+
+# In[ ]:
+
+
+
