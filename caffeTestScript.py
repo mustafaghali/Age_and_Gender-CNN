87ccae4e5a1f60bd9f0c2d21e4f4941d840c6194
mustafaghali <wad_alghali@hotmail.com>
2016-11-21 20:44:43 +0300

    Update caffeTestScript.py

--- caffeTestScript.py
+++ caffeTestScript.py
@@ -16,21 +16,21 @@ plt.rcParams['figure.figsize'] = (10, 10)
 plt.rcParams['image.interpolation'] = 'nearest'
 plt.rcParams['image.cmap'] = 'gray'
 
-mean_filename='./mean.binaryproto'
+mean_filename='../mean.binaryproto'
 proto_data = open(mean_filename, "rb").read()
 a = caffe.io.caffe_pb2.BlobProto.FromString(proto_data)
 mean  = caffe.io.blobproto_to_array(a)[0]
 
-age_net_pretrained='./age_net.caffemodel'
-age_net_model_file='./deploy_age.prototxt'
+age_net_pretrained='../age_net.caffemodel'
+age_net_model_file='../deploy_age.prototxt'
 age_net = caffe.Classifier(age_net_model_file, age_net_pretrained,
                        mean=mean,
                        channel_swap=(2,1,0),
                        raw_scale=255,
                        image_dims=(256, 256))
 
-gender_net_pretrained='./gender_net.caffemodel'
-gender_net_model_file='./deploy_gender.prototxt'
+gender_net_pretrained='../gender_net.caffemodel'
+gender_net_model_file='../deploy_gender.prototxt'
 gender_net = caffe.Classifier(gender_net_model_file, gender_net_pretrained,
                        mean=mean,
                        channel_swap=(2,1,0),
